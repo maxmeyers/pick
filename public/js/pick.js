b171b969ef183a751c4dc5a94ab8f5b86038f162
Max Meyers <maxmeyers@me.com>
2014-02-05 23:14:03 -0800

    wip

---
+++ public/js/pick.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+
+	$('button.get-started').click(function(event) {
+		document.location = "/start";
+	});
+}
